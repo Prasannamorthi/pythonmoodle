a622b4931253c48b71a20b976d65a621847433cc
Prasannamorthi <231001148@rajalakshmi.edu.in>
2024-06-15 15:04:59 +0530

    Update Week-5.02.py

--- Week-5/Week-5.02.py
+++ Week-5/Week-5.02.py
@@ -1,128 +1,36 @@
-Check pair with difference k
+Assume that the given string has enough memory.
 
-Given an array A of sorted integers and another non negative integer k, find if there exists 2 indices i and j such that A[i] - A[j] = k, i != j.
+ 
 
-Input Format
+Don't use any extra space(IN-PLACE)
 
-1.      First line is number of test cases T. Following T lines contain:
+ 
 
-2.      N, followed by N integers of the array
+Sample Input 1
 
-3.      The non-negative integer k
+ 
 
-Output format
+a2b4c6
 
-Print 1 if such a pair exists and 0 if it doesn’t
+ 
 
-Input
+Sample Output 1
 
-1
+ 
 
-3 
+aabbbbcccccc
 
-1 
 
-3 
-
-5
-
-4
-
-Output:
-
-1
-
-Input
-
-1
-
-3 
-
-1 
-
-3 
-
-5
-
-99
-
-Output
-
-0
-
-
-
-
-
-For example:
-
-Input	Result
-
-1
-
-3 
-
-1 
-
-3 
-
-5
-
-4
-
-	1
-
-1
-
-3 
-
-1 
-
-3 
-
-5
-
-99
-
-	0
-
-
-
-a=int(input())
-
-while(a!=0):
-
-    b=int(input())
-
-    l=[]
-
-    f=0
-
-    for i in range(b):
-
-        c=int(input())
-
-        l.append(c)
-
-    k=int(input())
-
-    a-=1
-
-    for i in range(b):
-
-        for j in range(b):
-
-            if(l[i]-l[j]==k and i!=j):
-
-                f=1
-
-                break
-
-    if(f==1):
-
-        print(1)
-
-    else:
-
-        print(0)
+s=input()
+result=''
+i=0
+while(i<len(s)):
+    char=s[i]
+    i+=1
+    num=''
+    while(i<len(s) and s[i].isdigit()):
+        num+=s[i]
+        i+=1
+    result=char*int(num)
+    print(result,end='')
 
