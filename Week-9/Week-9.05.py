9afdea6b01b0395fae7776aa945567fc2e8ea3d0
Prasannamorthi <231001148@rajalakshmi.edu.in>
2024-06-15 15:39:14 +0530

    Update Week-9.05.py

--- Week-9/Week-9.05.py
+++ Week-9/Week-9.05.py
@@ -1,66 +1,39 @@
-Scramble Score
+An e-commerce company plans to give their customers a special discount for Christmas.
 
+They are planning to offer a flat discount. The discount value is calculated as the sum of all
 
+the prime digits in the total bill amount.
 
-In the game of Scrabble™, each letter has points associated with it. The total score of a word is the sum of the scores of its letters. More common letters are worth fewer points while less common letters are worth more points. 
+Write an algorithm to find the discount value for the given total bill amount.
 
-Write a program that computes and displays the Scrabble™ score for a word. Create a dictionary that maps from letters to point values. Then use the dictionary to compute the score.
+Constraints
 
-A Scrabble™ board includes some squares that multiply the value of a letter or the value of an entire word. We will ignore these squares in this exercise.
+1 <= orderValue< 10e100000
 
+Input
 
+The input consists of an integer orderValue, representing the total bill amount.
 
-The points associated with each letter are shown below:
+Output
 
-Points Letters
+Print an integer representing the discount value for the given total bill amount.
 
-1 A, E, I, L, N, O, R, S, T and U
+Example Input
 
-2 D and G
+578
 
-3 B, C, M and P
+Output
 
-4 F, H, V, W and Y
+12
 
-5 K
+For example:
 
-8 J and X
-
-10 Q and Z
-
-
-
-Sample Input
-
-REC
-
-Sample Output
-
-REC is worth 5 points.
-
-
-
-letter_scores = {
-
-    'A': 1, 'E': 1, 'I': 1, 'L': 1, 'N': 1, 'O': 1, 'R': 1, 'S': 1, 'T': 1, 'U': 1,
-
-    'D': 2, 'G': 2,
-
-    'B': 3, 'C': 3, 'M': 3, 'P': 3,
-
-    'F': 4, 'H': 4, 'V': 4, 'W': 4, 'Y': 4,
-
-    'K': 5,
-
-    'J': 8, 'X': 8,
-
-    'Q': 10, 'Z': 10
-
-}
-
-word = input().upper()
-
-score = sum(letter_scores.get(letter, 0) for letter in word)
-
-print(word,"is worth",score,"points.")
-
+Test	Result
+print(christmasDiscount(578))
+12
+def christmasDiscount(n):
+  discount = 0
+  for digit in str(n):
+      if int(digit) in [2, 3, 5, 7]:
+         discount += int(digit)
+  return discount  
