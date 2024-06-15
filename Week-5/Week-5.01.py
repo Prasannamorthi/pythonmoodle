7d551dc275abbf447431f819122b591d8fd13793
Prasannamorthi <231001148@rajalakshmi.edu.in>
2024-06-15 15:03:52 +0530

    Update Week-5.01.py

--- Week-5/Week-5.01.py
+++ Week-5/Week-5.01.py
@@ -1,126 +1,34 @@
-Balanced Array
-
-Given an array of numbers, find the index of the smallest array element (the pivot), for which the sums of all elements to the left and to the right are equal. The array may not be reordered.
-
-Example
-
-arr=[1,2,3,4,6]
-
-·         the sum of the first three elements, 1+2+3=6. The value of the last element is 6.
-
-·         Using zero based indexing, arr[3]=4 is the pivot between the two subarrays.
-
-·         The index of the pivot is 3.
-
-Constraints
-
-·         3 ≤ n ≤ 105
-
-·         1 ≤ arr[i] ≤ 2 × 104, where 0 ≤ i < n
-
-·         It is guaranteed that a solution always exists.
-
-The first line contains an integer n, the size of the array arr.
-
-Each of the next n lines contains an integer, arr[i], where 0 ≤ i < n.
-
-Sample Case 0
-
-Sample Input 0
-
-4
-
-1
-
-2
-
-3
-
-3
-
-Sample Output 0
-
-2
-
-Explanation 0
-
-The sum of the first two elements, 1+2=3. The value of the last element is 3
-
-Using zero based indexing, arr[2]=3 is the pivot between the two subarrays
-
-The index of the pivot is 2
-
-Sample Case 1
-
-Sample Input 1
-
-3
-
-1
-
-2
-
-1
-
-Sample Output 1
-
-1
-
-Explanation 1
-
-The first and last elements are equal to 1
-
-Using zero based indexing, arr[1]=2 is the pivot between the two subarrays
-
-The index of the pivot is 1.
-
-
-
+Reverse a string without affecting special characters
+ Given a string S, containing special characters and all the alphabets, reverse the string without affecting the positions of the special characters.
+Input:
+A&B
+Output:
+B&A
+Explanation: As we ignore '&' and
+As we ignore '&' and then reverse, so answer is "B&A".
 
 
 For example:
 
 Input	Result
+A&x#
+x&A#
 
-4
-
-1
-
-2
-
-3
-
-3
-
-	2
-
-3
-
-1
-
-2
-
-1	1
-
-a=int(input())
-
+s=input()
 l=[]
-
-for i in range(a):
-
-    c=int(input())
-
-    l.append(c)
-
-for i in range(1,a):
-
-    d=sum(l[0:i])
-
-    r=sum(l[i+1:])
-
-    if(d==r):
-
-        print(i)
+for i in s:
+    if(i.isalpha()):
+        l.append(i)
+l.reverse()
+result=''
+index=0
+for i in s:
+    if(i.isalpha()):
+        result+=l[index]
+        index+=1
+    else:
+        result+=i
+print(result)
 
 
 
