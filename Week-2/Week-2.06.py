3606f4ad870a4a16299f6520b3a5caf6a95c5f7a
Prasannamorthi <231001148@rajalakshmi.edu.in>
2024-06-15 14:10:48 +0530

    Update Week-2.06.py

--- Week-2/Week-2.06.py
+++ Week-2/Week-2.06.py
@@ -1,40 +1,19 @@
-Eligible to donate blood
+An online retailer sells two products: widgets and gizmos. Each widget weighs 75 grams. Each gizmo weighs 112 grams. Write a program that reads the number of widgets and the number of gizmos from the user. Then your program should compute and display the total weight of the parts.
 
-A team from the Rotract club had planned to conduct a rally to create awareness among the Coimbatore people to donate blood. They conducted the rally successfully. Many of the Coimbatore people realized it and came forward to donate their blood to nearby blood banks. The eligibility criteria for donating blood are people should be above or equal to 18 and his/ her weight should be above 40. There was a huge crowd and staff in the blood bank found it difficult to manage the crowd. So they decided to keep a system and ask the people to enter their age and weight in the system. If a person is eligible he/she will be allowed inside.
+Sample Input:
 
- Write a program and feed it to the system to find whether a person is eligible or not.
+10
 
+20
 
+Sample Output:
 
-Input Format:
+The total weight of all these widgets and gizmos is 2990 grams.
 
-Input consists of two integers that correspond to the age and weight of a person respectively.
-
-Output Format:
-
-Display True(IF ELIGIBLE)
-
-Display False (if not eligible)
-
-Sample Input
-
-19
-
-45
-
-Sample Output
-
-True
 
 a=int(input())
-
 b=int(input())
-
-if(a>=18 and b>40):
-
-    print("True")
-
-else:
-
-    print("False")
+c=a*75
+d=b*112
+print("The total weight of all these widgets and gizmos is",c+d,"grams.")
 
