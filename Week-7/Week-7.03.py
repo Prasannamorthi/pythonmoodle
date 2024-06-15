091227e26876cddddba5b24c95d271d9782c27ae
Prasannamorthi <231001148@rajalakshmi.edu.in>
2024-06-15 15:22:16 +0530

    Update Week-7.03.py

--- Week-7/Week-7.03.py
+++ Week-7/Week-7.03.py
@@ -1,86 +1,76 @@
-Check Product of Digits
+Given an array of strings words, return the words that can be typed using letters of the alphabet on only one row of American keyboard like the image below.
 
-Write a code to check whether product of digits at even places is divisible by sum of digits at odd place of a positive integer.
+In the American keyboard:
 
-Input Format:
+the first row consists of the characters "qwertyuiop",
+the second row consists of the characters "asdfghjkl", and
+the third row consists of the characters "zxcvbnm".
 
+ 
 
+Example 1:
 
-Take an input integer from stdin.
+Input: words = ["Hello","Alaska","Dad","Peace"]
+Output: ["Alaska","Dad"]
+Example 2:
 
+Input: words = ["omk"]
+Output: []
+Example 3:
 
-
-Output Format:
-
-
-
-Print TRUE or FALSE.
-
-
-
-Example Input:
-
-
-
-1256
-
-
-
-Output:
-
-
-
-TRUE
-
-
-
-Example Input:
-
-
-
-1595
-
-
-
-Output:
-
-
-
-FALSE
-
+Input: words = ["adsdf","sfd"]
+Output: ["adsdf","sfd"]
 
 
 For example:
 
+Input	Result
+4
+Hello
+Alaska
+Dad
+Peace
+Alaska
+Dad
+2
+adsfd
+afd
+adsfd
+afd
 
 
-Test	Result
 
-print(productDigits(1256))	True
-
-print(productDigits(1595))	False
-
-
-
-def productDigits(n):
-
-    a=str(n)
-
-    s,p=0,1
-
-    for i in range(0,len(a),2):
-
-        s+=int(a[i])
-
-    for i in range(1,len(a),2):
-
-        p*=int(a[i])
-
-    if(p%s==0):
-
-        return("True")
-
-    else:
-
-        return("False")
-
+a=int(input())
+lst=[]
+for i in range(0,a):
+    b=input("")
+    lst.append(b)
+lst2=['q','w','e','r','t','y','u','i','o','p','Q','W','E','R','T','Y','U','I','O','P']
+lst3=['a','s','d','f','g','h','j','k','l','A','S','D','F','G','H','J','K','L']
+lst4=['z','x','c','v','b','n','m','Z','X','C','V','B','N','M']
+l=0
+m=0
+n=0
+lst5=[]
+for i in lst:
+    l=0
+    m=0
+    n=0
+    j=i
+    b=len(j)
+    for k in range(0,b):
+        if(i[k] not in lst3 and i[k] not in lst4):
+            l+=1
+        elif(i[k] not in lst2 and i[k] not in lst4):
+            m+=1
+        elif(i[k] not in lst2 and i[k] not in lst3):
+            n+=1
+    if(l==b or m==b or n==b):
+        lst5.append(i)
+p=0
+for i in lst5:
+    p+=1
+    if i!=0:
+        print(i)
+if(p==0):
+    print("No words")
