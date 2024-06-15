d2a225c91eb4908dddcc4ffb35b30ae277b97464
Prasannamorthi <231001148@rajalakshmi.edu.in>
2024-06-15 14:12:47 +0530

    Update Week-2.08.py

--- Week-2/Week-2.08.py
+++ Week-2/Week-2.08.py
@@ -1,38 +1,37 @@
-Troy Battle
+Pretend that you have just opened a new savings account that earns 4 percent interest per year. The interest that you earn is paid at the end of the year, and is added to the balance of the savings account. Write a program that begins by reading the amount of money deposited into the account from the user. Then your program should compute and display the amount in the savings account after 1, 2, and 3 years. Display each amount so that it is rounded to 2 decimal places. Sample Input: 10000 Sample Output: Balance as of end of Year 1: $10400.00. Balance as of end of Year 2: $10816.00. Balance as of end of Year 3: $11248.64.
+For example:
 
-In the 1800s, the battle of Troy was led by Hercules. He was a superstitious person. He believed that his crew can win the battle only if the total count of the weapons in hand is in multiple of 3 and the soldiers are in an even number of count. Given the total number of weapons and the soldier's count, Find whether the battle can be won or not according to Hercules's belief. If the battle can be won print True otherwise print False.
+Input	Result
+10000
+Balance as of end of Year 1: $10400.00.
+Balance as of end of Year 2: $10816.00.
+Balance as of end of Year 3: $11248.64.
+Pretend that you have just opened a new savings account that earns 4 percent interest per year. The interest that you earn is paid at the end of the year, and is added to the balance of the savings account. Write a program that begins by reading the amount of money deposited into the account from the user. Then your program should compute and display the amount in the savings account after 1, 2, and 3 years. Display each amount so that it is rounded to 2 decimal places. Sample Input: 10000 Sample Output: Balance as of end of Year 1: $10400.00. Balance as of end of Year 2: $10816.00. Balance as of end of Year 3: $11248.64.
+For example:
 
-Input format:
-
-Line 1 has the total number of weapons
-
-Line 2 has the total number of Soldiers.
-
-Output  Format:
-
-If the battle can be won print True otherwise print False.
-
-
-
-Sample Input:
-
-32
-
-43
-
-Sample Output:'
-
-False
+Input	Result
+10000
+Balance as of end of Year 1: $10400.00.
+Balance as of end of Year 2: $10816.00.
+Balance as of end of Year 3: $11248.64.
 
 
 
-a=int(input())
-
-b=int(input())
-
-if(a%3==0 and b%2==0):
-
-    print("True")
+def calculate_savings_account_balance(principal, interest_rate, years):
+    balance = principal
+    for year in range(1, years + 1):
+        interest = balance * interest_rate / 100
+        balance += interest
+        balance = round(balance, 2)
+    return balance
+def main():
+    principal = float(input())
+    interest_rate = 4
+    for years in [1, 2, 3]:
+        balance = calculate_savings_account_balance(principal, interest_rate, years)
+        print(f"Balance as of end of Year {years}: $""%.2f."%balance)
+if __name__ == "__main__":
+    main()
 
 else:
 
