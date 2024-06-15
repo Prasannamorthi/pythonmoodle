3eaa10e30ae80d18abee19f31f50c8f8f7622759
Prasannamorthi <231001148@rajalakshmi.edu.in>
2024-06-15 15:28:06 +0530

    Update Week-8.01.py

--- Week-8/Week-8.01.py
+++ Week-8/Week-8.01.py
@@ -1,43 +1,83 @@
-Binary String
+Given an array of names of candidates in an election. A candidate name in the array represents a vote cast to the candidate. Print the name of candidates received Max vote. If there is tie, print a lexicographically smaller name.
 
-Coders here is a simple task for you, Given string str. Your task is to check whether it is a binary string or not by using python set.
+Examples: 
+
+Input :  votes[] = {"john", "johnny", "jackie",
+
+                    "johnny", "john", "jackie",
+
+                    "jamie", "jamie", "john",
+
+                    "johnny", "jamie", "johnny",
+
+                    "john"};
+
+Output : John
+
+We have four Candidates with name as 'John', 'Johnny', 'jamie', 'jackie'. The candidates John and Johny get maximum votes. Since John is alphabetically smaller, we print it. Use dictionary to solve the above problem
+
+ 
+
+Sample Input:
+
+10
+
+John
+
+John
+
+Johny
+
+Jamie
+
+Jamie
+
+Johny
+
+Jack
+
+Johny
+
+Johny
+
+Jackie
+
+ 
+
+Sample Output:
+
+Johny
+
+ 
+
+ 
+
+ 
 
 
 
-Examples:  
-
-Input: str = "01010101010"
-
-Output: Yes
-
-
-
-Input: str = "REC101"
-
-Output: No
 
 
 
 
 
-For example:
-
-Input	Result
-
-01010101010	Yes
-
-010101 10101	No
 
 
+def find_winner(votes):
+    vote_count = {}
+    for name in votes:
+        if name in vote_count:
+            vote_count[name] += 1
+        else:
+            vote_count[name] = 1
+    
+    max_votes = max(vote_count.values())
+    winners = [name for name, votes in vote_count.items() if votes == max_votes]
+    winner = sorted(winners)[0]
+    
+    return winner
 
-a = input()
-
-try:
-
-    c = int(a)
-
-    print("Yes")
-
-except:
-
-    print("No")
+n = int(input())
+votes = [input() for _ in range(n)]
+winner = find_winner(votes)
+print(winner)
