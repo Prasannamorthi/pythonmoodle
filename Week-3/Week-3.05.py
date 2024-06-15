ab9ed1fd606096527cc37f3809e9e3a26db4dfcd
Prasannamorthi <231001148@rajalakshmi.edu.in>
2024-06-15 14:20:14 +0530

    Update Week-3.05.py

--- Week-3/Week-3.05.py
+++ Week-3/Week-3.05.py
@@ -1,6 +1,4 @@
-Vowel or Consonant
-
-In this exercise you will create a program that reads a letter of the alphabet from the user. If the user enters a, e, i, o or u then your program should display a message indicating that the entered letter is a vowel. If the user enters 'y' then your program should display a message indicating that sometimes y is a vowel, and sometimes y is a consonant. Otherwise your program should display a message indicating that the letter is a consonant.
+In this exercise you will create a program that reads a letter of the alphabet from the user. If the user enters a, e, i, o or u then your program should display a message indicating that the entered letter is a vowel. If the user enters y then your program should display a message indicating that sometimes y is a vowel, and sometimes y is a consonant. Otherwise your program should display a message indicating that the letter is a consonant.
 
 Sample Input 1
 
@@ -27,32 +25,20 @@ Sample Output 3
 It's a consonant.
 
 
-
-
-
 For example:
 
 Input	Result
-
-y	Sometimes it's a vowel... Sometimes it's a consonant.
-
-u	It's a vowel.
-
-p	It's a consonant.
+y
+Sometimes it's a vowel... Sometimes it's a consonant.
+c
+It's a consonant.
 
 
-
-a=input()
-
-if(a=='a' or a=='e' or a=='i' or a=='o' or a=='u'):
-
+_a=input()
+if(_a=='a' or _a=='e' or _a=='i' or _a=='o' or _a=='u'):
     print("It's a vowel.")
-
-elif(a=='y'):
-
+elif(_a=='y'):
     print("Sometimes it's a vowel... Sometimes it's a consonant.")
-
 else:
-
     print("It's a consonant.")
 
