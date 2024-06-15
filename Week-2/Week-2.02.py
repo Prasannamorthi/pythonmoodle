fdb592d46d3b4a67228d258ed5d522937040e947
Prasannamorthi <231001148@rajalakshmi.edu.in>
2024-06-15 14:07:50 +0530

    Update Week-2.02.py

--- Week-2/Week-2.02.py
+++ Week-2/Week-2.02.py
@@ -1,33 +1,52 @@
-Doll Sings
+Mr. X's birthday is in next month. This time he is planning to invite N of his friends. He wants to distribute some chocolates to all of his friends after the party. He went to a shop to buy a packet of chocolates. At the chocolate shop, 4 packets are there with different numbers of chocolates. He wants to buy such a packet which contains a number of chocolates, which can be distributed equally among all of his friends. Help Mr. X to buy such a packet.
 
-In London, every year during Dasara there will be a very grand doll show. People try to invent new dolls of different varieties. The best-sold doll's creator will be awarded with a cash prize. So people broke their heads to create dolls innovatively. Knowing this competition, Mr.Lokpaul tried to create a doll that sings only when an even number is pressed and the number should not be zero and greater than 100.
+ Input Given: 
 
- IF Lokpaul wins print true, otherwise false.
+N-No of friends
+
+P1,P2,P3 AND P4-No of chocolates
+
+OUTPUT:
+
+ "True" if he can buy that packet and "False" if he can't buy that packet.
+
+SAMPLE INPUT AND OUTPUT:
+
+5
+
+25
+
+12  
+
+10  
+
+9
+
+OUTPUT
+
+True False True False
 
 
 
-Sample Input
+For example:
 
+Input	Result
+5
+25
+23
+20
 10
-
-Sample Output
-
-True
-
-Explanation:
-
-Since 10 is an even number and a number between 0 and 100, True is printed
+True False True True
 
 
 
-a=int(input())
 
-if(a>0 and a<100 and a%2==0):
-
-    print("True")
-
-else:
-
-    print("False")
+n=int(input())
+for i in range(4):
+    a=int(input())
+    if(a%n==0):
+        print("True",end=' ')
+    else:
+        print("False",end=' ')
 
 
