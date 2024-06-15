d9d4342c018b9976460cf2045f83e666fe84ed7c
Prasannamorthi <231001148@rajalakshmi.edu.in>
2024-06-15 15:07:00 +0530

    Update Week-5.04.py

--- Week-5/Week-5.04.py
+++ Week-5/Week-5.04.py
@@ -1,150 +1,53 @@
-Distinct Elements in an Array
+Consider the below words as key words and check the given input is key word or not.
 
-Program to print all the distinct elements in an array. Distinct elements are nothing but the unique (non-duplicate) elements present in the given array.
+keywords: {break, case, continue, default, defer, else, for, func, goto, if, map, range, return, struct, type, var}
 
-Input Format:
+Input format:
 
-First line take an Integer input from stdin which is array length n.
+Take string as an input from stdin.
 
-Second line take n Integers which is inputs of array.
+Output format:
 
-Output Format:
-
-Print the Distinct Elements in Array in single line which is space Separated
-
-
+Print the word is key word or not.
 
 Example Input:
 
-5
-
-1 
-
-2 
-
-2 
-
-3 
-
-4
+break
 
 Output:
 
-1 2 3 4
+break is a keyword
 
 Example Input:
 
-6
-
-1 
-
-1 
-
-2 
-
-2 
-
-3 
-
-3
+IF
 
 Output:
 
-1 2 3
-
+IF is not a keyword
 
 
 For example:
 
 Input	Result
-
-5
-
-1 
-
-2 
-
-2 
-
-3
-
-4
-
-1 2 3 4 
-
-6
-
-1 
-
-1 
-
-2 
-
-2 
-
-3 
-
-3
-
-1 2 3
+break
+break is a keyword
+IF
+IF is not a keyword
 
 
 
 
 
-def merge_arrays_without_duplicates(arr1, arr2):
-
-    # Combine the arrays and convert to a set to remove duplicates
-
-    result_set = set(arr1 + arr2)
-
-    # Convert the set back to a sorted list
-
-    merged_sorted_array = sorted(result_set)
-
-    return merged_sorted_array
-
- 
-
-# Input read and processing
-
-def process_input():
-
-    # Reading number of elements and the elements for the first array
-
-    n1 = int(input())
-
-    array1 = []
-
-    for _ in range(n1):
-
-        element = int(input())
-
-        array1.append(element)
-
- 
-
-    # Reading number of elements and the elements for the second array
-
-    n2 = int(input())
-
-    array2 = []
-
-    for _ in range(n2):
-
-        element = int(input())
-
-        array2.append(element)
-
- 
-
-    # Merge the arrays without duplicates
-
-    result = merge_arrays_without_duplicates(array1, array2)
-
- 
-
-    # Print the result
-
-    print(" ".join(map(str, result)))
+keywords=['break','case','continue','default','defer','else','for','func','goto','if','map','range','return','struct','type','var']
+s=input()
+flag=0
+for i in keywords:
+    if(s==i):
+        flag=1
+        break
+if(flag==1):
+    print(s,'is a keyword')
+else:
+    print(s,'is not a keyword')
 
