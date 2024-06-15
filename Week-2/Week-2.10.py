951f717c2c5abbfa27360a62d9d1b2f7ec3ab2cc
Prasannamorthi <231001148@rajalakshmi.edu.in>
2024-06-15 14:15:10 +0530

    Update Week-2.10.py

--- Week-2/Week-2.10.py
+++ Week-2/Week-2.10.py
@@ -1,20 +1,41 @@
-Return last digit of the given number
+Note:
 
-Write a program that returns the last digit of the given number. Last digit is being referred to the least significant digit i.e. the digit in the ones (units) place in the given number.
+Dont use if-else. Operators alone must be used .
 
-The last digit should be returned as a positive number.
 
-For example,
 
-if the given number is 197, the last digit is 7
+A team from the Rotract club had planned to conduct a rally to create awareness among the Coimbatore people to donate blood. They conducted the rally successfully. Many of the Coimbatore people realized it and came forward to donate their blood to nearby blood banks. The eligibility criteria for donating blood are people should be above or equal to 18 and his/ her weight should be above 40. There was a huge crowd and staff in the blood bank found it difficult to manage the crowd. So they decided to keep a system and ask the people to enter their age and weight in the system. If a person is eligible he/she will be allowed inside.
 
-if the given number is -197, the last digit is 7
+ Write a program and feed it to the system to find whether a person is eligible or not.
+
+ Input Format:
+
+ Input consists of two integers that correspond to the age and weight of a person respectively.
+
+ Output Format:
+
+ Display True(IF ELIGIBLE)
+
+Display False (if not eligible)
+
+Sample Input
+
+19
+
+45
+
+Sample Output
+
+True
+
+
 
 For example:
 
 Input	Result
-
-123	3
+18
+40
+False
 
 
 
@@ -23,6 +44,9 @@ Input	Result
 
 
 a=int(input())
-
-print(abs(a)%10)
+b=int(input())
+if(a>=18 and b>40):
+    print("True")
+else:
+    print("False")
 
